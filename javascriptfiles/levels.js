f7201250473a3efcb84f35daac6dcad4939fab97
Yuinshiel <wee.jiawei.kevan@dhs.sg>
2018-05-09 19:59:38 +0800

    Update levels.js

--- javascriptfiles/levels.js
+++ javascriptfiles/levels.js
@@ -3,12 +3,12 @@ app = angular.module("dhsiccquiz");
 app.factory("levels", function() {
   var levels = [
     {
-      minimum_score: 0,
+      minimun_score: 0,
   		title: "Sorry!",
   		text: "You did not get enough answers correct to get a stamp."
   	},
     {
-      minimum_score: 3,
+      minimun_score: 3,
   		title: "Congratulations!",
   		text: "You got a stamp!"
   	},
@@ -18,7 +18,7 @@ app.factory("levels", function() {
   return {
 		getLevel: function(score) {
       filtered = levels.filter(function(result) {
-        return level.minimum_score >= score;
+        return level.minimun_score >= score;
       });
 
       return filtered[0];
